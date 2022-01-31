1499dc1cbd53cc8c3f9b9c63fafb81d0d167662f
vishal <vishalsingh21397@gmail.com>
2022-01-31 10:59:33 +0530

    pushing updated code

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using BulkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
